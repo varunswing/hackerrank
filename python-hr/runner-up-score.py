9030c68d729d797d170b9a256074c6f289d56b99
varunswing <varuns939@gmail.com>
2019-08-28 23:13:48 +0530

    Update runner-up-score.py

--- python-hr/runner-up-score.py
+++ python-hr/runner-up-score.py
@@ -1,3 +1,14 @@
-if __name__ == '__main__':
-    n = int(input())
-    arr = map(int, input().split())
+n = int(input())
+arr = list(map(int, input().split()))
+
+for i in range(n-1):
+    for i in range(n-1):
+        if(arr[i] > arr[i+1]):
+            c = arr[i]
+            arr[i] = arr[i+1]
+            arr[i+1] = c
+
+for i in range(n):
+    if(arr[n-1] > arr [n-i-1]):
+        print(arr[n-i-1])
+        break
